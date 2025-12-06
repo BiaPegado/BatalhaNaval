02649205e0bd091bf07d9728651429d9bec0754b
kali <kali@localhost>
2025-12-06 10:36:55 +0000

    Adiciona JML do Destroyer

--- src/br/ufrn/imd/model/Destroyer.java
+++ src/br/ufrn/imd/model/Destroyer.java
@@ -13,14 +13,24 @@ import java.util.ArrayList;
  */
 public class Destroyer extends Navio {
 
+    //@ public invariant mTamanho == 5;
+
 	/**
 	 * Construtor do Destroyer. Recebe uma lista de posicoes iniciais e uma direcao
 	 * (horizontal ou vertical) para o posicionamento inicial do navio.
 	 *
 	 * @param posicao Lista de posicoes iniciais onde o Destroyer sera posicionado
-	 *                no tabuleiro.
+	 * no tabuleiro.
 	 * @param direcao Direcao inicial do navio (0 para horizontal, 1 para vertical).
 	 */
+    /*@ 
+      @ requires posicao != null;
+      @ requires posicao.size() == 5; 
+      @ requires direcao == 0 || direcao == 1;
+      @ ensures mTamanho == 5;
+      @ ensures this.posicao == posicao;
+      @ ensures this.direcao == direcao;
+      @*/
 	public Destroyer(ArrayList<String> posicao, int direcao) {
 		super(5, posicao, direcao);
 	}
