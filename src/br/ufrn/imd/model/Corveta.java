65888f1dd673658ac94c32e5048aa7eb874802cc
kali <kali@localhost>
2025-12-06 16:43:22 +0000

    Ajeita Corveta

--- src/br/ufrn/imd/model/Corveta.java
+++ src/br/ufrn/imd/model/Corveta.java
@@ -7,9 +7,16 @@ import java.util.ArrayList;
  */
 public class Corveta extends Navio {
 
+	public static final int TAMANHO = 2;
     //@ public invariant mTamanho == 2;
 
     /**
+     * Construtor da Corveta.
+     *
+     * @param posicao Lista de posicoes iniciais.
+     * @param direcao Direcao inicial (0 ou 1).
+     */
+	/**
      * Construtor da Corveta.
      *
      * @param posicao Lista de posicoes iniciais.
@@ -20,10 +27,12 @@ public class Corveta extends Navio {
       @ requires posicao.size() == 2; 
       @ requires direcao == 0 || direcao == 1;
       @ ensures mTamanho == 2;
+      @ ensures this.tamanho == 2;
       @ ensures this.posicao == posicao;
       @ ensures this.direcao == direcao;
+      @ pure
       @*/
     public Corveta(ArrayList<String> posicao, int direcao) {
-        super(2, posicao, direcao);
+        super(TAMANHO, posicao, direcao);
     }
 }
