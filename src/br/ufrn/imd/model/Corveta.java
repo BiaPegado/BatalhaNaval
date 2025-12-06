9cbeca7e30243401579b41f0e623138f9b6f4420
kali <kali@localhost>
2025-12-06 10:28:23 +0000

    Adiciona JML da corveta

--- src/br/ufrn/imd/model/Corveta.java
+++ src/br/ufrn/imd/model/Corveta.java
@@ -3,25 +3,27 @@ package br.ufrn.imd.model;
 import java.util.ArrayList;
 
 /**
- * Representa uma Corveta na batalha naval. E uma subclasse de {@link Navio},
- * especificamente projetada para representar a Corveta, que e um tipo de navio
- * com tamanho fixo de 2 unidades.
- * <p>
- * Esta classe herda atributos e comportamentos da classe {@link Navio},
- * incluindo a capacidade de ser posicionada no tabuleiro e de ser atingida por
- * tiros.
+ * Representa uma Corveta na batalha naval.
  */
 public class Corveta extends Navio {
 
-	/**
-	 * Construtor da Corveta. Recebe uma lista de posicoes iniciais e uma direcao
-	 * (horizontal ou vertical) para o posicionamento inicial do navio.
-	 *
-	 * @param posicao Lista de posicoes iniciais onde a Corveta sera posicionada no
-	 *                tabuleiro.
-	 * @param direcao Direcao inicial do navio (0 para horizontal, 1 para vertical).
-	 */
-	public Corveta(ArrayList<String> posicao, int direcao) {
-		super(2, posicao, direcao);
-	}
+    //@ public invariant mTamanho == 2;
+
+    /**
+     * Construtor da Corveta.
+     *
+     * @param posicao Lista de posicoes iniciais.
+     * @param direcao Direcao inicial (0 ou 1).
+     */
+    /*@ 
+      @ requires posicao != null;
+      @ requires posicao.size() == 2; 
+      @ requires direcao == 0 || direcao == 1;
+      @ ensures mTamanho == 2;
+      @ ensures this.posicao == posicao;
+      @ ensures this.direcao == direcao;
+      @*/
+    public Corveta(ArrayList<String> posicao, int direcao) {
+        super(2, posicao, direcao);
+    }
 }
